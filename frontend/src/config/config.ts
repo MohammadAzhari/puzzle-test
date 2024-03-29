ef54d7b991e37e20f28fa89e25097dd2cf19f448
Mohammad Azhari <mazharioffical@gmail.com>
2024-03-29 15:49:28 +0400

    default url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.API_URL || "http://localhost:4000/",
+  baseURL: import.meta.env.API_URL || "http://161.35.202.152:4000/",
   headers: {
     "Content-Type": "application/json",
   },
